7643ad2f4c341671e3ad218413aefeecee0a776b
Peter Bryant <peter.bryant@thekeysupport.com>
2019-12-18 14:28:55 +0000

    add blog page linked with graphql

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,22 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Portfolio 2020',
+  siteUrl: 'http://portfolio.dev.local',
+  host: 'portfolio.dev.local',
+  port: 5200,
+  plugins: [
+    {
+      use: '@gridsome/source-graphql',
+      options: {
+        url: 'http://portfolio.dev.local:9200/graphql',
+        fieldName: 'wagtail',
+        typeName: 'wagtailTypes',
+
+        headers: {
+          Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+        },
+      },
+    },
+  ]
 }
---
+++ src/pages/Blog.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <h1>Blog posts</h1>
+    <div v-for="blog in $page.wagtail.pages" :key="blog.id">
+      <h2>{{ blog.title }}</h2>
+      <span v-html="blog.body"></span>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  wagtail {
+    pages {
+      ...on wagtailTypes_BlogPage {
+        id
+        title
+        date
+        body
+      }
+    }
+  } 
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Blog'
+  }
+}
+</script>
