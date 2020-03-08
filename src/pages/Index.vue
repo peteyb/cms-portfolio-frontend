2dfb9e8d8e91267a55397145443d53bb8de4fb9d
Peter Bryant <peter.bryant@thekeysupport.com>
2020-03-08 22:57:04 +0000

    update concept list

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -16,16 +16,20 @@
         <li>Consuming Wagtail data via graphql - including pagination</li>
         <li>Consuming Wagtail data via DRF - including pagination</li>
         <li>Handling wagtail private pages and fields</li>
-        <li>Backend authentication of SSG build phase</li>
+        <li>Backend authentication of SSG build phase (TokenAuthentication)</li>
         <li>Webhook to build site after content edit in Wagtail</li>
         <li>Load test Django Wagtail pages versus Gridsome SSG equivalents</li>
         <li>Have dynamic functionality in SSG i.e. search for posts</li>
-        <li>Have a vuex store</li>
-        <li>Handle authentication on search</li>
+        <li>Have a vuex store; handles search and extra content loads on private pages</li>
+        <li>Handle authentication on search (TokenAuthentication from localStorage but could be from Cookie or JWT)</li>
         <li>Handle image uploading to Cloudinary through Wagtail</li>
         <li>Handle image rendering from Cloudinary through Gridsome Vue.js</li>
         <li>Use Bootstrap framework in frontend</li>
         <li>Add GA to site</li>
+        <li>Updated Wagtail API to return if Page is restricted</li>
+        <li>Updated Wagtail API to exclude specific (<em>extra</em>) fields where Page is private</li>
+        <li>Updated Gridsome to pre-render all pages but only display extra content on public pages</li>
+        <li>Updated Gridsome to dynamically load extra content on private pages where user is authenticated</li>
       </ol>
     </p>
 
